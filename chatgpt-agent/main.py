a19131eee21bbb49c73c50d8bc34b2ce2512c84a
jinwook song <wlsdnr129@naver.com>
2025-09-29 22:02:03 +0900

    feat(chatgpt): hosted mcp tool

--- chatgpt-agent/main.py
+++ chatgpt-agent/main.py
@@ -11,6 +11,7 @@ from agents import (
     FileSearchTool,
     ImageGenerationTool,
     CodeInterpreterTool,
+    HostedMCPTool,
 )
 
 dotenv.load_dotenv()
@@ -52,6 +53,15 @@ if "agent" not in st.session_state:
                     "container": {
                         "type": "auto",
                     },
+                }
+            ),
+            HostedMCPTool(
+                tool_config={
+                    "server_url": "https://mcp.context7.com/mcp",
+                    "type": "mcp",
+                    "server_label": "Context7",
+                    "server_description": "Use this to get the docs from software projects.",
+                    "require_approval": "never",
                 }
             ),
         ],
@@ -100,6 +110,14 @@ async def paint_history():
             elif message_type == "code_interpreter_call":
                 with st.chat_message("ai"):
                     st.code(message["code"])
+            elif message_type == "mcp_list_tools":
+                with st.chat_message("ai"):
+                    st.write(f"Listed {message['server_label']}'s tools")
+            elif message_type == "mcp_call":
+                with st.chat_message("ai"):
+                    st.write(
+                        f"Called {message['server_label']}'s {message['name']} with args {message['arguments']}"
+                    )
 
 
 asyncio.run(paint_history())
@@ -139,11 +157,44 @@ def update_status(status_container, event):
             "🎨 Drawing image...",
             "running",
         ),
-        "response.code_interpreter_call_code.done": ("🤖 Ran code", "complete"),
-        "response.code_interpreter_call.completed": ("🤖 Ran code", "complete"),
-        "response.code_interpreter_call.in_progress": ("🤖 Running code...", "running"),
+        "response.code_interpreter_call_code.done": (
+            "🤖 Ran code.",
+            "complete",
+        ),
+        "response.code_interpreter_call.completed": (
+            "🤖 Ran code.",
+            "complete",
+        ),
+        "response.code_interpreter_call.in_progress": (
+            "🤖 Running code...",
+            "complete",
+        ),
         "response.code_interpreter_call.interpreting": (
             "🤖 Running code...",
+            "complete",
+        ),
+        "response.mcp_call.completed": (
+            "⚒️ Called MCP tool",
+            "complete",
+        ),
+        "response.mcp_call.failed": (
+            "⚒️ Error calling MCP tool",
+            "complete",
+        ),
+        "response.mcp_call.in_progress": (
+            "⚒️ Calling MCP tool...",
+            "running",
+        ),
+        "response.mcp_list_tools.completed": (
+            "⚒️ Listed MCP tools",
+            "complete",
+        ),
+        "response.mcp_list_tools.failed": (
+            "⚒️ Error listing MCP tools",
+            "complete",
+        ),
+        "response.mcp_list_tools.in_progress": (
+            "⚒️ Listing MCP tools",
             "running",
         ),
         "response.completed": ("", "complete"),
