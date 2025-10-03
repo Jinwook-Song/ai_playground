017bc53a36df5e788cedb64b6d7cfab2a2742991
jinwook song <wlsdnr129@naver.com>
2025-10-03 16:01:59 +0900

    feat(financial-analyst): tools, sub agents

--- financial-analyst/financial_advisor/agent.py
+++ financial-analyst/financial_advisor/agent.py
@@ -3,13 +3,26 @@ from google.adk.models.lite_llm import LiteLlm
 
 
 MODEL = LiteLlm("openai/gpt-5-nano-2025-08-07")
-# MODEL = LiteLlm("openai/gpt-4o")
+
+
+def get_weather(city: str):
+    return f"The weather in {city} is sunny."
+
+
+geo_agent = Agent(
+    name="GeoAgent",
+    instruction="You help the user with geography related questions",
+    model=MODEL,
+    description="Transfer to this agent if the user asks about geography related questions",
+)
 
 
 weather_agent = Agent(
     name="WeatherAgent",
     instruction="You help the user with weather related questions",
     model=MODEL,
+    tools=[get_weather],
+    sub_agents=[geo_agent],
 )
 
 
