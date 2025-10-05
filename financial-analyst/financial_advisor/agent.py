2fb607b6b47c45922d8836f696dfda8fde6e98bb
jinwook song <wlsdnr129@naver.com>
2025-10-05 23:57:39 +0900

    feat(financial-analyst): agent state

--- financial-analyst/financial_advisor/agent.py
+++ financial-analyst/financial_advisor/agent.py
@@ -1,3 +1,4 @@
+from google.adk.tools import ToolContext
 from google.adk.agents import Agent
 from google.adk.tools.agent_tool import AgentTool
 from google.adk.models.lite_llm import LiteLlm
@@ -6,11 +7,31 @@ from .sub_agents.financial_analyst import financial_analyst
 from .sub_agents.news_analyst import news_analyst
 from .prompt import PROMPT
 
-MODEL = LiteLlm("openai/gpt-5-nano-2025-08-07")
+MODEL = LiteLlm("openai/gpt-4o")
 
 
-def save_advice_report():
-    pass
+def save_advice_report(tool_context: ToolContext, summary: str):
+    state = tool_context.state
+    data_analyst_result = state.get("data_analyst_result")
+    financial_analyst_result = state.get("financial_analyst_result")
+    news_analyst_analyst_result = state.get("news_analyst_analyst_result")
+    report = f"""
+        # Excetuve Summary and Advice:
+        {summary}
+
+        ## Data Analyst Report:
+        {data_analyst_result}
+
+        ## Financial Analyst Report:
+        {financial_analyst_result}
+        
+        ## News Analyst Report:
+        {news_analyst_analyst_result}
+    """
+    state["report"] = report
+    return {
+        "success": True,
+    }
 
 
 financial_advisor = Agent(
