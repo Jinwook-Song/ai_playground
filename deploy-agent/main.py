e047cca4c275f0f20b880bce5803198c41e52f4c
jinwook song <wlsdnr129@naver.com>
2026-01-15 22:48:12 +0900

    feat: stream response

--- deploy-agent/main.py
+++ deploy-agent/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from openai import AsyncOpenAI
 from pydantic import BaseModel
 
@@ -51,3 +52,40 @@ async def create_message(
     return {
         "answer": answer.final_output,
     }
+
+
+@app.post("/conversations/{conversation_id}/message/stream")
+async def create_message_stream(
+    conversation_id: str, input: CreateMessageInput
+) -> CreateMessageOutput:
+    async def event_generator():
+        events = Runner.run_streamed(
+            starting_agent=agent,
+            input=input.question,
+            conversation_id=conversation_id,
+        )
+        async for event in events.stream_events():
+            if (
+                event.type == "raw_response_event"
+                and event.data.type == "response.output_text.delta"
+            ):
+                yield event.data.delta
+
+    return StreamingResponse(event_generator(), media_type="text/plain")
+
+
+@app.post("/conversations/{conversation_id}/message/stream/all")
+async def create_message_stream_all(
+    conversation_id: str, input: CreateMessageInput
+) -> CreateMessageOutput:
+    async def event_generator():
+        events = Runner.run_streamed(
+            starting_agent=agent,
+            input=input.question,
+            conversation_id=conversation_id,
+        )
+        async for event in events.stream_events():
+            if event.type == "raw_response_event":
+                yield f"{event.data.to_json()}\n"
+
+    return StreamingResponse(event_generator(), media_type="text/plain")
